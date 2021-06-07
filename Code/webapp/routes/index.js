78e21df6843914e61274b8eb864ece4e95a0d7ba
BigFish2086 <63132227+BigFish2086@users.noreply.github.com>
2021-06-07 23:59:16 +0200

    Last Update

--- Code/webapp/routes/index.js
+++ Code/webapp/routes/index.js
@@ -25,26 +25,30 @@ router.get('/', function(req, res, next) {
 });
 
 // select * from indexed GROUP by word having COUNT(word) > 30
+// art content user zoom ahmed
 router.get('/results', function(req, res, next) {
   let word = String(req.query.search).toLowerCase();
   let stword = natural.PorterStemmer.stem(word);
-  
-  let sql = `SET @res1 = (SELECT LOG((Select Count(*) From Indexed) / (SELECT Count(*) from indexed as I, crawled as C where Word = ? and C.URL = I.URL))); select Word, C.Title, I.URL, I.Content, I.TF from indexed as I, crawled as C where Word = ? and C.URL = I.URL Order by I.TF * @res1 DESC;`;
   
   let notFoundObj = {
     state: "NOT FOUND",
     message: "Please Check your spelling .."
   };
 
-  con.query(sql, [stword, stword], (err, rows) => {
+  let sql = `SELECT LOG((Select Count(*) From Indexed) / (1 + (SELECT Count(*) from indexed where Word = ?))) as idf`;
+  con.query(sql, [stword], (err, idfObj) => {
     if(err) throw err;
-    //res.send({retData: rows});
-    let gtotalPages = parseInt(Math.ceil(rows.length / 10.0));
-    if(gtotalPages) {
-      res.render("results", {gtotalPages: gtotalPages, retData: rows});
-    } else {
-      res.render("results", {gtotalPages: 1, notFoundObj: [notFoundObj]});
-    }
+    let sql2 = `select Word, C.Title, I.URL, I.Content, I.TF from indexed as I, crawled as C where Word = ? and C.URL = I.URL Order by I.TF * ? DESC`;
+    con.query(sql2, [stword, parseFloat(idfObj[0].idf)], (err2, rows) => {
+      if(err2) throw err2;
+      //res.send({retData: rows});
+      let gtotalPages = parseInt(Math.ceil(rows.length / 10.0));
+      if(gtotalPages) {
+        res.render("results", {gtotalPages: gtotalPages, retData: rows});
+      } else {
+        res.render("results", {gtotalPages: 1, notFoundObj: [notFoundObj]});
+      }
+    });
   });
 });
 
